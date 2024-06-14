ff3f01703e7241dee9b592c0967b1e4d07624607
Carlos Carballo <159819105+Metaler127@users.noreply.github.com>
2024-06-14 03:39:37 -0700

    otra prueba

--- src/components/home/cards/cards.jsx
+++ src/components/home/cards/cards.jsx
@@ -37,7 +37,7 @@ function MyPlantCard({ publicacion }) {
     <div
       className="bg-white  lex flex-row flex-wrap max-w-sm rounded overflow-hidden shadow-lg"
       onClick={(e) => {
-        window.location.href = `https://herbarioapi-production-7512.up.railway.app/api/planta${publicacion.id}`;
+        window.location.href = `api/planta/${publicacion.id}`;
       }}
       role="button"
     >
